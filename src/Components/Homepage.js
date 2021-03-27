c908452a2fdcea9abd1e37f5308eccfc03b2f340
T__R__P__53 <tyler_1701@hotmail.com>
2021-03-27 18:36:40 -0400

    updated

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import mainImg from '../assets/images/Hired.PNG'
+import mainImg from '../assets/images/helloworld.png'
 import dashboard from '../assets/images/Dashboard.PNG'
 import homepageimg from '../assets/images/Homepage.PNG'
 import btpic from '../assets/images/budget.PNG'
@@ -35,8 +35,8 @@ function Homepage() {
         </nav>
 
         <section className="intro">
-            <h1 className="section-title title-intro">Hi, I'm <strong>Tyler Petri</strong></h1>
-            <p className="subtitle subtitle-intro">Web Developping Engineer</p>
+            <h1 className="section-title title-intro">Hi, I'm <span>Tyler Petri</span></h1>
+            <p className="subtitle subtitle-intro">Web Developing Engineer</p>
             <img src={mainImg} alt="Profile" className="my-pic"/>
         </section>
 
@@ -55,7 +55,7 @@ function Homepage() {
                         <h4>Full stack restful API & mySQL </h4>
                         <p>Mood tracker! Login, create a note (prompted if not logged-in, too many characters, etc), check your latest notes (edit, delete) and view data on calendar or donut graph! </p>
                         <p>Note: heroku bug regarding mySQL date feature</p>
-                        <a href='https://github.com/Humble-Attackers/Group-Mood-Tracker.git' target="_blank" rel="noreferrer" className="gitlink">Github</a>
+                        <a href='https://github.com/Humble-Attackers/Group-Mood-Tracker.git' target="_blank" rel="noreferrer" className="gitlink">Github repo</a>
                     </a>
                 </div>
                 <div className="work">
@@ -63,7 +63,7 @@ function Homepage() {
                         <img src={gmegif} alt="get-me-employed"/>
                         <h4>3rd party API</h4>
                         <p>Job search front end API app. Enter details, save whichever you're interested in and click the cards to be redirected to their site!</p>
-                        <a href='https://github.com/TeamCircleAlpha/GetMeEmployed.git' target="_blank" rel="noreferrer" className="gitlink">Github</a>
+                        <a href='https://github.com/TeamCircleAlpha/GetMeEmployed.git' target="_blank" rel="noreferrer" className="gitlink">Github repo</a>
                     </a>
                 </div>
                 <div className="work">
@@ -71,7 +71,7 @@ function Homepage() {
                         <img src={homepageimg} alt="employee-directory"/>
                         <h4>React app</h4>
                         <p>Your employee's information all in one place. Need to reach someone? Filter by name and/or city, boom. Get'er done.</p>
-                        <a href='https://github.com/TylerPetri/Employee-Directory.git' target="_blank" rel="noreferrer" className="gitlink">Github</a>
+                        <a href='https://github.com/TylerPetri/Employee-Directory.git' target="_blank" rel="noreferrer" className="gitlink">Github repo</a>
                     </a>
                 </div>
                 <div className="work">
@@ -79,7 +79,7 @@ function Homepage() {
                         <img src={btpic} alt="budget-tracker"/>
                         <h4>Service worker, IndexedDB & Mongoose</h4>
                         <p>Can I afford another coffee? Probably! Time to check the budget tracker 2000! Enter a name, a value, and selected whether it's an expense or deposit!</p>
-                        <a href='https://github.com/TylerPetri/Budget-tracker2000.git' target="_blank" rel="noreferrer" className="gitlink">Github</a>
+                        <a href='https://github.com/TylerPetri/Budget-tracker2000.git' target="_blank" rel="noreferrer" className="gitlink">Github repo</a>
                     </a>
                 </div>
                 <div className="work">
@@ -87,7 +87,7 @@ function Homepage() {
                         <img src={dashboard} alt="workout-tracker"/>
                         <h4>MongoDB</h4>
                         <p>You want to track your workouts? Maybe fool your loved ones? You can track weights, the name of the exercise, the type, the duration, the reps, the sets, and the distance! All neatly displayed in the dashboard page the the form of various graphs!</p>
-                        <a href='https://github.com/TylerPetri/Workout-Tracker-MongoDB.git' target="_blank" rel="noreferrer" className="gitlink">Github</a>
+                        <a href='https://github.com/TylerPetri/Workout-Tracker-MongoDB.git' target="_blank" rel="noreferrer" className="gitlink">Github repo</a>
                     </a>
                 </div>
                 <div className="work">
@@ -95,7 +95,7 @@ function Homepage() {
                         <img src={burgers} alt="burger-app"/>
                         <h4>Handlebars & mySQL</h4>
                         <p>Want a double double? Type it in the box and submit! Devour or not to devour is now the question.</p>
-                        <a href='https://github.com/TylerPetri/burger.git' target="_blank" rel="noreferrer" className="gitlink">Github</a>
+                        <a href='https://github.com/TylerPetri/burger.git' target="_blank" rel="noreferrer" className="gitlink">Github repo</a>
                     </a>
                 </div>
                 <div className="work">
@@ -103,7 +103,7 @@ function Homepage() {
                         <img src={gif} alt="employee-tracker"/>
                         <h4>Inquirer</h4>
                         <p>An application to track employee information within a database. There are options such as: adding roles, adding managers, seeing the utilized budget per department, etc.</p>
-                        <a href='https://github.com/TylerPetri/Employee-Tracker2.0.git' target="_blank" rel="noreferrer" className="gitlink">Github</a>
+                        <a href='https://github.com/TylerPetri/Employee-Tracker2.0.git' target="_blank" rel="noreferrer" className="gitlink">Github repo</a>
                     </a>
                 </div>
                
@@ -119,12 +119,12 @@ function Homepage() {
             <ul className="social-list">
                 <li className="social-item">
                     <a href={linkedin} className="social-link" target="_blank" rel="noreferrer">
-                        <SiLinkedin className="icons"/>
+                        <SiLinkedin className="icons" id="purple"/>
                     </a>
                 </li>
                 <li className="social-item">
                     <a href={github} className="social-link" target="_blank" rel="noreferrer">
-                        <FaGithub className="icons"/>
+                        <FaGithub className="icons"  id="purple"/>
                     </a>
                 </li>
             </ul>
